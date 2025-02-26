2a95ee9b17930b7e8305a9881dd91c05d8abfcb3
arhamansari11 <arhamansari454647@gmail.com>
2025-02-26 22:23:33 +0500

    Basics 5 Essentials Functions Done

--- Basics/Functions.py
+++ Basics/Functions.py
@@ -12,7 +12,7 @@ img = cv.imread(r"D:\Github\Open CV\Basics\Reading Images and Videos\Photos\park
 
 # 2. Convert the Image to Blur
 
-Blur = cv.GaussianBlur(img , (7 , 7)  , cv.BORDER_DEFAULT)
+# Blur = cv.GaussianBlur(img , (7 , 7)  , cv.BORDER_DEFAULT)
 
 # cv.imshow("Blue Image" , Blur)
 
@@ -20,16 +20,32 @@ Blur = cv.GaussianBlur(img , (7 , 7)  , cv.BORDER_DEFAULT)
 
 # 3. Edge Cascade
 
-canny = cv.Canny(Blur , 125 , 175)
+# canny = cv.Canny(Blur , 125 , 175)
 
-cv.imshow("Canny Edges" , canny)
+# cv.imshow("Canny Edges" , canny)
 
 # cv.waitKey(0)
 
 # 4. Dilating the Images
 
-dilate = cv.dilate(canny , (3 , 3) , iterations = 3)
+# dilate = cv.dilate(canny , (3 , 3) , iterations = 3)
 
-cv.imshow("Dilated" , dilate)
+# cv.imshow("Dilated" , dilate)
 
-cv.waitKey(0)
+# cv.waitKey(0)
+
+# 5. Resize The Image
+
+# resizde = cv.resize(img , (500 , 500) , interpolation = cv.INTER_CUBIC)
+
+# cv.imshow("Resized" , resizde)
+
+# cv.waitKey(0)
+
+# 6. Crop the Image
+
+# cropped = img[50:200 , 200:400]
+# cv.imshow("Cropped" , cropped)
+# cv.waitKey(0)
+
+

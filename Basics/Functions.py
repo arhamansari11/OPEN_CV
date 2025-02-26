0c847c3dd9a6dddf013cac0e9c32f714f7f443c6
arhamansari11 <arhamansari454647@gmail.com>
2025-02-26 20:37:57 +0500

    Adding 2 Basic Functions of OPEN CV

--- Basics/Functions.py
+++ Basics/Functions.py
@@ -1,11 +1,19 @@
 import cv2 as cv
 
-img = cv.imread(r"D:\Github\Open CV\Basics\Reading Images and Videos\Photos\group 2.jpg")
+img = cv.imread(r"D:\Github\Open CV\Basics\Reading Images and Videos\Photos\park.jpg")
 
-# Convert the Image to Gray Image
+# 1. Convert the Image to Gray Image
 
 # gray = cv.cvtColor(img , cv.COLOR_BGR2GRAY)
 
 # cv.imshow("Image" , gray)
+
+# cv.waitKey(0)
+
+# 2. Convert the Image to Blur
+
+# Blur = cv.GaussianBlur(img , (1 , 1)  , cv.BORDER_DEFAULT)
+
+# cv.imshow("Blue Image" , Blur)
 
 # cv.waitKey(0)

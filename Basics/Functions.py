5a612e8eeab0e6586d1a79127a402b526936340f
arhamansari11 <arhamansari454647@gmail.com>
2025-02-26 20:28:13 +0500

    Basics Functions In OPEN CV added

---
+++ Basics/Functions.py
@@ -0,0 +1,11 @@
+import cv2 as cv
+
+img = cv.imread(r"D:\Github\Open CV\Basics\Reading Images and Videos\Photos\group 2.jpg")
+
+# Convert the Image to Gray Image
+
+# gray = cv.cvtColor(img , cv.COLOR_BGR2GRAY)
+
+# cv.imshow("Image" , gray)
+
+# cv.waitKey(0)

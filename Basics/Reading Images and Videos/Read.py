f0e2a82ca8704922c0ea7c25855030afabd7360c
arhamansari11 <arhamansari454647@gmail.com>
2025-02-26 14:41:43 +0500

    Read Image and Video Added

--- Basics/Reading Images and Videos/Read.py
+++ Basics/Reading Images and Videos/Read.py
@@ -1,12 +1,38 @@
 import cv2 as cv
 
-# Use double backslashes or raw string to fix the path
-img = cv.imread(r"D:\Github\Open CV\Reading Images and Videos\Photos\cats 2.jpg")
 
-# Check if the image was loaded successfully
-if img is None:
-    print("Error: Image not found. Check the file path.")
-else:
-    cv.imshow("Cat Image", img)
-    cv.waitKey(0)
-    cv.destroyAllWindows()  # Ensure the window closes properly
+# For Reading Image
+
+# # Use double backslashes or raw string to fix the path
+# img = cv.imread(r"D:\Github\Open CV\Basics\Reading Images and Videos\Photos\cats 2.jpg")
+
+# # Check if the image was loaded successfully
+# if img is None:
+#     print("Error: Image not found. Check the file path.")
+# else:
+#     cv.imshow("Cat Image", img)
+#     cv.waitKey(0)
+#     cv.destroyAllWindows()  # Ensure the window closes properly
+
+
+# For Reading Video
+
+# Video Path
+
+capture = cv.VideoCapture(r"D:\Github\Open CV\Basics\Reading Images and Videos\Videos\dog.mp4")
+
+# Read the Image Frame by Frame
+
+while True:
+    isTrue , frame = capture.read()
+
+    cv.imshow("Video"  , frame)
+
+    if cv.waitKey(20) & 0xFF == ord("a"):
+        break
+
+
+capture.release()
+cv.destroyAllWindows()  
+
+

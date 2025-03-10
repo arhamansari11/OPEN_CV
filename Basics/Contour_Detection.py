4b0ec87af5eb4ab5e4e58428ed8c710e9d01a0fb
arhamansari11 <arhamansari454647@gmail.com>
2025-03-10 13:16:52 +0500

    Contour_Detection

--- Basics/Contour_Detection.py
+++ Basics/Contour_Detection.py
@@ -1,21 +1,31 @@
 import cv2 as cv
+import numpy as np
 
 img = cv.imread(r"D:\Github\Open CV\Basics\Reading Images and Videos\Photos\cats.jpg")
 
+blank = np.zeros(img.shape, dtype = "uint8")
+cv.imshow("Blank" , blank)
 
 gray = cv.cvtColor(img , cv.COLOR_BGR2GRAY)
 
-blur = cv.GaussianBlur(gray , (5 , 5) , cv.BORDER_DEFAULT)
+# blur = cv.GaussianBlur(gray , (5 , 5) , cv.BORDER_DEFAULT)
 
+# canny = cv.Canny(blur , 125 , 125)
 
+# cv.imshow("Canny Image" , canny)
 
-canny = cv.Canny(blur , 125 , 125)
+# ThresHolding on Image
 
-cv.imshow("Canny Image" , canny)
+# ret , thresh = cv.threshold(gray , 125 , 255 , cv.THRESH_BINARY)
+# cv.imshow("Thres Image" , thresh)
 
-contours , hierarchies = cv.findContours(canny , cv.RETR_LIST , cv.CHAIN_APPROX_SIMPLE)
+contours , hierarchies = cv.findContours(thresh , cv.RETR_LIST , cv.CHAIN_APPROX_SIMPLE)
 
 print(f'{len(contours)} contour(s) found!')
 
+cv.drawContours(blank , contours , -1 , (0 , 0 , 255) , 3)
+cv.imshow("Contours Drawn" , blank)
+
 cv.waitKey(0)
+
 

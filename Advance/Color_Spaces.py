5f7c228f1b7e8d0a2f1fcbf6b38ebd00fa08f9d5
arhamansari11 <arhamansari454647@gmail.com>
2025-03-10 14:37:31 +0500

    Color Spaces Complete

--- Advance/Color_Spaces.py
+++ Advance/Color_Spaces.py
@@ -1,7 +1,37 @@
 import cv2 as cv
+import matplotlib.pyplot as plt
 
-img = cv.imread(r"D:\Github\Open CV\Basics\Reading Images and Videos\Photos\boston.jpg")
+img = cv.imread(r"D:\Github\Open CV\Basics\Reading Images and Videos\Photos\cats.jpg")
 
 cv.imshow("Image" , img)
+
+# plt.imshow(img)
+# plt.show()
+
+# BGR to GRAY Scale
+
+gray = cv.cvtColor(img , cv.COLOR_BGR2GRAY)
+
+# BGR TO HSV (Huge Saturation Value)
+
+hsv = cv.cvtColor(img , cv.COLOR_BGR2HSV)
+
+# BGR TO LAB
+
+lab = cv.cvtColor(img , cv.COLOR_BGR2LAB)
+
+# cv.imshow("Lab Image" , lab)
+
+# BGR to RGB
+
+# rgb = cv.cvtColor(img , cv.COLOR_BGR2RGB)
+
+# cv.imshow("RGB" , rgb)
+
+# HSV TO BGR
+
+hsv_bgr = cv.cvtColor(hsv, cv.COLOR_HSV2BGR)
+
+cv.imshow("HSV TO BGR"  , hsv_bgr)
 
 cv.waitKey(0)

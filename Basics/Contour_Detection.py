2651e82c10bc6468deba01ef71a188822c7ab68a
arhamansari11 <arhamansari454647@gmail.com>
2025-03-10 12:33:43 +0500

    Contour_Detection

--- Basics/Contour_Detection.py
+++ Basics/Contour_Detection.py
@@ -2,6 +2,20 @@ import cv2 as cv
 
 img = cv.imread(r"D:\Github\Open CV\Basics\Reading Images and Videos\Photos\cats.jpg")
 
-cv.imshow("Cat Image" , img)
+
+gray = cv.cvtColor(img , cv.COLOR_BGR2GRAY)
+
+blur = cv.GaussianBlur(gray , (5 , 5) , cv.BORDER_DEFAULT)
+
+
+
+canny = cv.Canny(blur , 125 , 125)
+
+cv.imshow("Canny Image" , canny)
+
+contours , hierarchies = cv.findContours(canny , cv.RETR_LIST , cv.CHAIN_APPROX_SIMPLE)
+
+print(f'{len(contours)} contour(s) found!')
 
 cv.waitKey(0)
+

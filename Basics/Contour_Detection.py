fa47742f42ef0a1e22c26c7935c6ef196f07ff29
arhamansari11 <arhamansari454647@gmail.com>
2025-03-10 11:21:46 +0500

    Contour_Detection

--- Basics/Contour_Detection.py
+++ Basics/Contour_Detection.py
@@ -1,13 +1,7 @@
 import cv2 as cv
 
 img = cv.imread(r"D:\Github\Open CV\Basics\Reading Images and Videos\Photos\cats.jpg")
-cv.imshow("Cats" , img)
 
-gray = cv.cvtColor(img , cv.COLOR_BGR2GRAY)
-cv.imshow("Gray" , gray)
-
-Canny = cv.Canny(img , 125 , 175)
-cv.imshow("Canny Image" , Canny)
-
+cv.imshow("Cat Image" , img)
 
 cv.waitKey(0)

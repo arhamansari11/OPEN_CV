1b1d5d43bfc3881fa61783f652de19bff2f3f860
arhamansari11 <arhamansari454647@gmail.com>
2025-03-11 22:23:17 +0500

    Computing Histogtam

--- Advance/Histogram.py
+++ Advance/Histogram.py
@@ -1,24 +1,48 @@
 import cv2 as cv
 import matplotlib.pyplot as plt    
+import numpy as np
 
-img = cv.imread(r"D:\Github\Open CV\Basics\Reading Images and Videos\Photos\cats.jpg")
-
-cv.imshow("Image" , img)
-
-gray = cv.cvtColor(img , cv.COLOR_BGR2GRAY)
-
-cv.imshow("Gray Image" , gray)
+img = cv.imread(r"D:\Github\Open CV\Basics\Reading Images and Videos\Photos\cats 2.jpg")
+# cv.imshow("Image" , img)
 
 
-gray_hist = cv.calcHist([gray] , [0] , None , [256] , [0 , 256])
+# Gray Computing Histogram
+
+# gray = cv.cvtColor(img , cv.COLOR_BGR2GRAY)
+# # cv.imshow("Gray Image" , gray)
+
+
+# blank = np.zeros(img.shape[:2] , dtype = "uint8")
+# circle = cv.circle(blank , (img.shape[1] // 2 , img.shape[0] // 2) , 100 , 255 , -1)
+
+# mask = cv.bitwise_and(gray , gray , mask=circle)
+
+# cv.imshow("Masking" , mask) 
+
+
+# gray_hist = cv.calcHist([gray] , [0] , mask , [256] , [0 , 256])
+
+# plt.figure()
+# plt.title("GrayScale Histogram")
+# plt.xlabel("Bins")
+# plt.ylabel("# of pixels.")
+# plt.plot(gray_hist)
+# plt.xlim([0 , 256])
+# plt.show()
+
+
+# Color Computing Histogram
 
 plt.figure()
-plt.title("GrayScale Histogram")
+plt.title("ColorScale Histogram")
 plt.xlabel("Bins")
 plt.ylabel("# of pixels.")
-plt.plot(gray_hist)
-plt.xlim([0 , 256])
-plt.show()
+colors = ('b' , 'g' , 'r')
+for i , col in enumerate(colors):
+    hist = cv.calcHist([img] , [i] , None , [256] , [0 , 256])
+    plt.plot(hist , color = col)
+    plt.xlim([0 , 256])
 
+plt.show()
 
 cv.waitKey(0)

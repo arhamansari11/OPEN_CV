d480e7229db112302ed82c6c6ac0d30e4d54d9b0
arhamansari11 <arhamansari454647@gmail.com>
2025-03-11 09:55:35 +0500

    Bitwise Operations

--- Advance/Bitwise.py
+++ Advance/Bitwise.py
@@ -2,6 +2,6 @@ import cv2 as cv
 
 img = cv.imread(r"D:\Github\Open CV\Basics\Reading Images and Videos\Photos\cats.jpg")
 
-cv.imshow("Image"  , img)
+cv.imshow("Image Show"  , img)
 
 cv.waitKey(0)

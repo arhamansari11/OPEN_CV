ad583533d7be4d9ba5e33aa9a8dc43878e0900ea
arhamansari11 <arhamansari454647@gmail.com>
2025-03-12 11:37:05 +0500

    Thresholding in OPEN CV

--- Advance/Thresholding.py
+++ Advance/Thresholding.py
@@ -5,9 +5,19 @@ img = cv.imread(r"D:\Github\Open CV\Basics\Reading Images and Videos\Photos\cats
 gray = cv.cvtColor(img , cv.COLOR_BGR2GRAY)
 # cv.imshow("Gray Image" , gray)
 
-threshold , thresh = cv.threshold(gray , 100 , 250 , cv.THRESH_BINARY)
+# Simple Thresholding
+
+threshold , thresh = cv.threshold(gray , 150 , 250 , cv.THRESH_BINARY)
 cv.imshow("Simple Thresholding" , thresh)
 
 
+threshold , thresh_inv = cv.threshold(gray , 150 , 250 , cv.THRESH_BINARY_INV)
+cv.imshow("Simple Thresholding Inv" , thresh_inv)
+
+
+# Adaptive Thresholding
+
+adaptive_thresh = cv.adaptiveThreshold(gray , 255 , cv.ADAPTIVE_THRESH_MEAN_C , cv.THRESH_BINARY , 11 , 3)
+cv.imshow("Adaptive Thresholding" , adaptive_thresh)
 
 cv.waitKey(0)

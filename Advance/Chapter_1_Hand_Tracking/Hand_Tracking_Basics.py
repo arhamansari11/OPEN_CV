eec7e1b988850f3d27f90a1e3bbc343771ba98c0
arhamansari11 <arhamansari454647@gmail.com>
2025-03-13 22:40:43 +0500

    Advance Module / Chapter 1 / Hand Module Basics

---
+++ Advance/Chapter_1_Hand_Tracking/Hand_Tracking_Basics.py
@@ -0,0 +1,42 @@
+import cv2 as cv
+import mediapipe as mp
+import time
+
+cap = cv.VideoCapture(0)
+
+mphands = mp.solutions.hands
+hands = mphands.Hands()
+mpDraw = mp.solutions.drawing_utils
+
+pTime = 0
+cTime = 0
+
+while True:
+    success , img = cap.read()
+
+    imgRGB = cv.cvtColor(img , cv.COLOR_BGR2RGB)
+    results = hands.process(imgRGB)
+
+    if results.multi_hand_landmarks:
+        for handlms in results.multi_hand_landmarks:
+            for id , lm in enumerate(handlms.landmark):
+                # print(id , lm)
+                h , w , c = img.shape
+                cx , cy = int(lm.x * w) , int(lm.y * h)
+                print(id , cx  , cy)
+                if id == 8:
+                    cv.circle(img , (cx , cy) , 25 , (255 , 0 , 255) , -1)
+            mpDraw.draw_landmarks(img , handlms , mphands.HAND_CONNECTIONS)
+
+    cTime = time.time()
+    fps = 1 / (cTime - pTime)
+    pTime = cTime
+
+    cv.putText(img , str(int(fps)) , (10 , 70) , cv.FONT_HERSHEY_PLAIN , 3 , (255 , 0 , 255) , 3)
+
+    cv.imshow("Image" , img)
+    if cv.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv.destroyAllWindows()

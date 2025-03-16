839595c62e22966295b76a2bbdec7e15068f2168
arhamansari11 <arhamansari454647@gmail.com>
2025-03-16 21:14:49 +0500

    Chapter_3_Face_Detection

--- Advance/Chapter_3_Face_Detection/Face_Detection.py
+++ Advance/Chapter_3_Face_Detection/Face_Detection.py
@@ -1,3 +1,19 @@
 import cv2 as cv
 import mediapipe as mp
-import time
+import time 
+
+cap = cv.VideoCapture(0)
+pTime = 0
+
+
+
+while True:
+    success , img = cap.read()
+
+    cTime = time.time()
+    fps = 1 / (cTime - pTime)
+    pTime = cTime
+
+    cv.putText(img , f'FPS : {int(fps)}' , (20 , 70) , cv.FONT_HERSHEY_PLAIN , 3 ,(255 , 0 , 0) , 2)
+    cv.imshow("Video" , img)
+    cv.waitKey(1)
